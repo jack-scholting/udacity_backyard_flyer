6b111d1f750f2099fe9c9eaba41ead28e6b10895
siriusjs <siriusjs49@gmail.com>
2020-04-18 13:46:54 -0500

    checkin

--- backyard_flyer.py
+++ backyard_flyer.py
@@ -187,21 +187,12 @@ class BackyardFlyer(Drone):
 
 
 if __name__ == "__main__":
-    conn = MavlinkConnection('tcp:127.0.0.1:5760', threaded=False, PX4=False)
-    #conn = WebSocketConnection('ws://127.0.0.1:5760')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', type=int, default=5760, help='Port number')
+    parser.add_argument('--host', type=str, default='127.0.0.1', help="host address, i.e. '127.0.0.1'")
+    args = parser.parse_args()
+
+    conn = MavlinkConnection('tcp:{0}:{1}'.format(args.host, args.port), threaded=False, PX4=False)
     drone = BackyardFlyer(conn)
     time.sleep(2)
     drone.start()
-
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('--port', type=int, default=5760, help='Port number')
-    # parser.add_argument('--host', type=str, default='127.0.0.1', help="host address, i.e. '127.0.0.1'")
-    # args = parser.parse_args()
-
-    # conn = MavlinkConnection('tcp:{0}:{1}'.format(args.host, args.port), threaded=False, PX4=False)
-    # conn = MavlinkConnection('tcp:127.0.0.1:5760', threaded=False, PX4=False)
-    # drone = BackyardFlyer(conn)
-    # time.sleep(2)
-    # drone.start()
-    # drone.arming_transition()
-    # drone.target_position()
